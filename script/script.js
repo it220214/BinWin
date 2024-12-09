39028bf7a7de142fd1d2b774a352fce9007f43ef
Lehner Sebastian <basti2006@gmx.at>
2024-12-09 17:49:05 +0100

    Navigation

---
+++ script/script.js
@@ -0,0 +1,32 @@
+//Navigation
+function changeSideTo(side) {
+    switch (side) {
+        case 'ranking':
+            rankSystem();
+            break;
+        case 'statistic':
+            statisticSystem();
+            break;
+        case 'points':
+            pointsystem();
+            break;
+        case 'rewards':
+            rewardSystem();
+            break;
+            case 'profile':
+            profileSystem();
+            break;    
+        default:
+            break;
+    }
+}
+function rankSystem() {
+}
+function statisticSystem() {
+}
+function pointsystem() {
+}
+function rewardSystem() {
+}
+function profileSystem() {
+}
