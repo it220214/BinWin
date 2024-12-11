d4a1a890b5de0e42e6e14f896b9b46e556949442
Lehner Sebastian <basti2006@gmx.at>
2024-12-11 08:55:10 +0100

    logIn

--- script/script.js
+++ script/script.js
@@ -1,5 +1,27 @@
+//variablen 
+content = document.getElementById('#content');
+
+//valid-Login
+function validLogIn(){
+    document.body.innerHTML = `
+    <div id="headerGeneral">
+        <img id="logo" src="./Logo_BinWin.png"> 
+    </div>
+
+    <div id="content"></div>
+
+    <footer>
+        <img onclick="changeSideTo('ranking')" class="icons" src="./icons/ranking.svg">
+        <img onclick="changeSideTo('statistic')" class="icons" src="./icons/statistic.svg">
+        <p onclick="changeSideTo('points')" class="icons">♻️</p>
+        <img onclick="changeSideTo('rewards')" class="icons" src="./icons/trophy.svg">
+        <img onclick="changeSideTo('profile')" class="icons" src="./icons/profile.svg">
+    </footer>`
+}
+
 //Navigation
 function changeSideTo(side) {
+    content.innerhtml = "";
     switch (side) {
         case 'ranking':
             rankSystem();
